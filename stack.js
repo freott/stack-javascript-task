1ace38ddf6fefeb732074fbfa9c3a673cd6526b2
Fredrik Ottosson <fredrik.ottosson@knowit.se>
2021-05-14 16:29:26 +0200

    Add Stack with full functionality

--- stack.js
+++ stack.js
@@ -1,1 +1,25 @@
 // Your implementation goes here
+
+function Stack(...items) {
+  const list = items
+
+  const isEmpty = () => !list.length
+  const pop = () => isEmpty() ? null : list.pop()
+  const peek = () => list[list.length - 1]
+  const push = (item) => { list.push(item) }
+  const swap = () => {
+    const [lastItem, secondLastItem] = [pop(), pop()]
+    list.push(lastItem, secondLastItem)
+  }
+
+  return {
+    get size() { return list.length },
+    isEmpty,
+    pop,
+    peek,
+    push,
+    swap,
+  }
+}
+
+module.exports = Stack
